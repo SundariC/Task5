9c86270389350e832c3f67d737c578f2ad96e2f9
Sundari <csundari2000@gmail.com>
2025-11-02 21:27:13 +0530

    completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,12 +3,12 @@ import { TiShoppingCart } from "react-icons/ti";
 
 const Navbar = ({cartCount, onCartClick}) => {
   return (
-    <nav className="navbar bg-indigo-200 p-6 text-white fixed w-full top-0 z-10 shadow-md mb-4">
+    <nav className="navbar bg-gradient-to-r from-blue-500 to-teal-500 p-6 text-white fixed w-full top-0 z-10 shadow-md mb-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="brand text-lg text-sky-700 font-bold">MY STORE</div>
+        <div className="brand text-lg text-white font-bold">MY STORE</div>
         <div className="nav-actions">
           <button
-            className="bg-sky-700 px-4 py-2 rounded cart-btn flex items-center hover:outline-none focus:outline-none text-indigo-200"
+            className="bg-white px-4 py-2 rounded cart-btn flex items-center hover:outline-none focus:outline-none text-indigo-200"
             onClick={onCartClick}
           >
             <TiShoppingCart className="text-2xl" />
--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -3,7 +3,8 @@ import ProductCard from './ProductCard';
 
 const ProductList = ({ products, addToCart }) => {
   return (
-    <div className="container bg-black mt-20 p-8 rounded-lg w-full">
+    <div className="product-list bg-emerald-200 min-h-screen pt-10">
+    <div className="container mx-auto mt-20 p-6">
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
       {products.map((ele) => (
         <ProductCard
@@ -12,6 +13,7 @@ const ProductList = ({ products, addToCart }) => {
           addToCart={addToCart}
         />
       ))}
+    </div>
     </div>
     </div>
   );
